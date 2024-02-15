b455d5f3275abcf6715df06f1a5415d1b164e2ef
Julek <31626592+dnjulek@users.noreply.github.com>
2024-02-15 02:37:30 -0300

    add ``RFS``

---
+++ build.zig
@@ -0,0 +1,27 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const lib = b.addSharedLibrary(.{
+        .name = "vszip",
+        .root_source_file = .{ .path = "src/vszip.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const vapoursynth_dep = b.dependency("vapoursynth", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
+    lib.root_module.addImport("vapoursynth", vapoursynth_dep.module("vapoursynth"));
+    lib.linkLibC();
+
+    if (lib.root_module.optimize == .ReleaseFast) {
+        lib.root_module.strip = true;
+    }
+
+    b.installArtifact(lib);
+}
---
+++ powershell/RunWithBAT.ps1
@@ -0,0 +1,33 @@
+$jsonUrl = "https://raw.githubusercontent.com/ziglang/www.ziglang.org/master/data/releases.json"
+$jsonContent = Invoke-RestMethod -Uri $jsonUrl -Method Get
+$version = $jsonContent.master.version
+$zipUrl = $jsonContent.master.'x86_64-windows'.tarball
+
+$parentFolder = Split-Path -Path $PSScriptRoot -Parent
+$zipFilePath = Join-Path $PSScriptRoot "zig-windows-x86_64-$version.zip"
+$zigPath = Join-Path $PSScriptRoot "\zig-windows-x86_64-$version\zig.exe"
+$dllFilePath = Join-Path $parentFolder "\zig-out\lib\vszip.dll"
+$destinationFileFolder = Join-Path $env:APPDATA "\VapourSynth\plugins64"
+New-Item -ItemType Directory -Force -Path $destinationFileFolder | Out-Null
+$destinationFilePath = Join-Path $destinationFileFolder "\vszip.dll"
+
+if (-not (Test-Path $zipFilePath)){
+    Write-Host "Downloading zig-windows-x86_64-$version.zip..." -ForegroundColor Green
+    $ProgressPreference = 'SilentlyContinue'
+    Invoke-WebRequest -Uri $zipUrl -OutFile $zipFilePath
+}
+
+if (-not (Test-Path $zigPath)){
+    Write-Host "Extracting zig-windows-x86_64-$version.zip..." -ForegroundColor Green
+    Expand-Archive -Path $zipFilePath -DestinationPath $PSScriptRoot -Force
+}
+
+Write-Host "Building vszip.dll..." -ForegroundColor Green
+& $zigPath build -Doptimize=ReleaseFast
+
+if (Test-Path $dllFilePath){
+    Write-Host "Installing 'vszip.dll' to '$destinationFilePath'" -ForegroundColor Blue
+    Copy-Item -Path $dllFilePath -Destination $destinationFilePath -Force
+} else {
+    Write-Host "No '\zig-out\lib\vszip.dll' file, build error." -ForegroundColor Red
+}
