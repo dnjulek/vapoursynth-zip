fdafb6502c437e2eb55f87cca5888df728fec981
Julek <31626592+dnjulek@users.noreply.github.com>
2024-02-17 22:56:59 -0300

    RFS: ``clip_a`` -> ``clipa`` args

--- src/vszip.zig
+++ src/vszip.zig
@@ -8,5 +8,5 @@ pub const vsh = vapoursynth.vshelper;
 export fn VapourSynthPluginInit2(plugin: *vs.Plugin, vspapi: *const vs.PLUGINAPI) void {
     _ = vspapi.configPlugin.?("com.julek.zip", "zip", "Zig Image Process", vs.makeVersion(1, 0), vs.VAPOURSYNTH_API_VERSION, 0, plugin);
     _ = vspapi.registerFunction.?(pavg.filter_name, "clipa:vnode;exclude:int[];clipb:vnode:opt;planes:int[]:opt;", "clip:vnode;", pavg.planeAverageCreate, null, plugin);
-    _ = vspapi.registerFunction.?(rfs.filter_name, "clip_a:vnode;clip_b:vnode;frames:int[];mismatch:int:opt;planes:int[]:opt;", "clip:vnode;", rfs.rfsCreate, null, plugin);
+    _ = vspapi.registerFunction.?(rfs.filter_name, "clipa:vnode;clipb:vnode;frames:int[];mismatch:int:opt;planes:int[]:opt;", "clip:vnode;", rfs.rfsCreate, null, plugin);
 }
