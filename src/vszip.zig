7f92288e82c3da3a6478f181313c389220c459de
Julek <31626592+dnjulek@users.noreply.github.com>
2024-02-20 02:28:03 -0300

    add ``PlaneMinMax``

--- src/vszip.zig
+++ src/vszip.zig
@@ -1,6 +1,7 @@
 const vapoursynth = @import("vapoursynth");
 const rfs = @import("rfs.zig");
 const pavg = @import("planeaverage.zig");
+const pmm = @import("planeminmax.zig");
 
 pub const vs = vapoursynth.vapoursynth4;
 pub const vsh = vapoursynth.vshelper;
@@ -8,5 +9,6 @@ pub const vsh = vapoursynth.vshelper;
 export fn VapourSynthPluginInit2(plugin: *vs.Plugin, vspapi: *const vs.PLUGINAPI) void {
     _ = vspapi.configPlugin.?("com.julek.zip", "zip", "Zig Image Process", vs.makeVersion(1, 0), vs.VAPOURSYNTH_API_VERSION, 0, plugin);
     _ = vspapi.registerFunction.?(pavg.filter_name, "clipa:vnode;exclude:int[];clipb:vnode:opt;planes:int[]:opt;", "clip:vnode;", pavg.planeAverageCreate, null, plugin);
+    _ = vspapi.registerFunction.?(pmm.filter_name, "clipa:vnode;minthr:float:opt;maxthr:float:opt;clipb:vnode:opt;planes:int[]:opt;", "clip:vnode;", pmm.planeMinMaxCreate, null, plugin);
     _ = vspapi.registerFunction.?(rfs.filter_name, "clipa:vnode;clipb:vnode;frames:int[];mismatch:int:opt;planes:int[]:opt;", "clip:vnode;", rfs.rfsCreate, null, plugin);
 }
