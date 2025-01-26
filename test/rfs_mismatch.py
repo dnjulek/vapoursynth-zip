129c2dda55b8f2a42ab0409c423cac756d6334cc
dnjulek <dnjulek@hotmail.com>
2025-01-26 16:51:24 -0300

    Fix RFS test (windows)

--- test/rfs_mismatch.py
+++ test/rfs_mismatch.py
@@ -1,10 +1,8 @@
 import vapoursynth as vs
 core = vs.core
 
-core.std.LoadPlugin("../zig-out/bin/vszip.dll")
-
 clip1 = core.std.BlankClip(format=vs.YUV420P8, width=1280, height=720, fpsnum=24000, fpsden=1001, length=10)
 clip2 = core.std.BlankClip(format=vs.YUV420P16, width=1920, height=1080, fpsnum=30000, fpsden=1, length=10)
 
-rfs = core.vszip.RFS(clip1, clip2, mismatch=True)
+rfs = core.vszip.RFS(clip1, clip2, frames=[0, 3, 5, 1], mismatch=True)
 print(rfs)
