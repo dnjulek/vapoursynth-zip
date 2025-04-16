fd7b102e05373406211c292dfb216bac4fde6a3e
julek <dnjulek@hotmail.com>
2025-04-16 15:04:03 -0300

    update rfs_mismatch.py  test

--- test/rfs_mismatch.py
+++ test/rfs_mismatch.py
@@ -5,4 +5,10 @@ clip1 = core.std.BlankClip(format=vs.YUV420P8, width=1280, height=720, fpsnum=24
 clip2 = core.std.BlankClip(format=vs.YUV420P16, width=1920, height=1080, fpsnum=30000, fpsden=1, length=10)
 
 rfs = core.vszip.RFS(clip1, clip2, frames=[0, 3, 5, 1], mismatch=True)
+
+assert(rfs.width == 0)
+assert(rfs.height == 0)
+assert(rfs.fps == 0)
+assert(rfs.format == None)
+
 print(rfs)
