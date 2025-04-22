f204764dab5e26d224d3aed63f222216be7ce2d0
julek <dnjulek@hotmail.com>
2025-04-22 17:08:45 -0300

    Update test

--- test/rfs_mismatch.py
+++ test/rfs_mismatch.py
@@ -1,5 +1,9 @@
 import vapoursynth as vs
 core = vs.core
+import os
+
+path = "./../zig-out/lib/libvszip.so" if (os.name == "posix")  else "./../zig-out/bin/vszip.dll"
+core.std.LoadPlugin(path)
 
 clip1 = core.std.BlankClip(format=vs.YUV420P8, width=1280, height=720, fpsnum=24000, fpsden=1001, length=10)
 clip2 = core.std.BlankClip(format=vs.YUV420P16, width=1920, height=1080, fpsnum=30000, fpsden=1, length=10)
