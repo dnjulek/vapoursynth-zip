e64a4d279d4905e1c79e8f50a3b9846867713fa6
dnjulek <dnjulek@hotmail.com>
2025-05-16 17:15:57 -0300

    Update tests

---
+++ test/test_all.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+vspipe -p ./adaptive_binarize.vpy .
+vspipe -p ./bilateral.vpy .
+vspipe -p ./boxblur.vpy .
+vspipe -p ./checkmate.vpy .
+vspipe -p ./clahe.vpy .
+vspipe -p ./color_map.vpy .
+vspipe -p ./comb_mask_mt.vpy .
+vspipe -p ./metrics_ssimu2.vpy .
+vspipe -p ./plane_props.vpy .
+vspipe -p ./rfs_mismatch.vpy .
