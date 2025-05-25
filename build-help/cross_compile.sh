726507d251da09333e84ac8726ec5d76cafdb889
dnjulek <dnjulek@hotmail.com>
2025-05-25 18:27:54 -0300

    R7

--- build-help/cross_compile.sh
+++ build-help/cross_compile.sh
@@ -5,22 +5,22 @@ rm -rf ./zig-out ./.zig-cache ./build
 mkdir build
 
 zig build -Doptimize=ReleaseFast -Dtarget=x86_64-windows -Dcpu=x86_64_v3
-zip -9 -j build/vapoursynth-zip-r6-windows-x86_64.zip zig-out/bin/vszip.dll
+zip -9 -j build/vapoursynth-zip-r7-windows-x86_64.zip zig-out/bin/vszip.dll
 
 zig build -Doptimize=ReleaseFast -Dtarget=x86_64-windows -Dcpu=znver3
-zip -9 -j build/vapoursynth-zip-r6-windows-x86_64-znver3.zip zig-out/bin/vszip.dll
+zip -9 -j build/vapoursynth-zip-r7-windows-x86_64-znver3.zip zig-out/bin/vszip.dll
 
 zig build -Doptimize=ReleaseFast -Dtarget=x86_64-windows -Dcpu=znver4
-zip -9 -j build/vapoursynth-zip-r6-windows-x86_64-znver4.zip zig-out/bin/vszip.dll
+zip -9 -j build/vapoursynth-zip-r7-windows-x86_64-znver4.zip zig-out/bin/vszip.dll
 
 zig build -Doptimize=ReleaseFast -Dtarget=x86_64-macos
-zip -9 -j build/vapoursynth-zip-r6-macos-x86_64.zip zig-out/lib/libvszip.dylib
+zip -9 -j build/vapoursynth-zip-r7-macos-x86_64.zip zig-out/lib/libvszip.dylib
 
 zig build -Doptimize=ReleaseFast -Dtarget=aarch64-macos
-zip -9 -j build/vapoursynth-zip-r6-macos-aarch64.zip zig-out/lib/libvszip.dylib
+zip -9 -j build/vapoursynth-zip-r7-macos-aarch64.zip zig-out/lib/libvszip.dylib
 
 zig build -Doptimize=ReleaseFast -Dtarget=x86_64-linux-gnu.2.17 -Dcpu=x86_64_v3
-zip -9 -j build/vapoursynth-zip-r6-linux-gnu-x86_64.zip zig-out/lib/libvszip.so
+zip -9 -j build/vapoursynth-zip-r7-linux-gnu-x86_64.zip zig-out/lib/libvszip.so
 
 zig build -Doptimize=ReleaseFast -Dtarget=aarch64-linux-gnu.2.17
-zip -9 -j build/vapoursynth-zip-r6-linux-gnu-aarch64.zip zig-out/lib/libvszip.so
+zip -9 -j build/vapoursynth-zip-r7-linux-gnu-aarch64.zip zig-out/lib/libvszip.so
