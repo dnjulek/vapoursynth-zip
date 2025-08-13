6e5f38815b5857aa1f81284fa119a8696f40aecf
dnjulek <dnjulek@hotmail.com>
2025-08-13 09:36:10 -0300

    bump

--- src/vszip.zig
+++ src/vszip.zig
@@ -25,7 +25,7 @@ export fn VapourSynthPluginInit2(plugin: *vs.Plugin, vspapi: *const vs.PLUGINAPI
         "com.julek.vszip",
         "vszip",
         "VapourSynth Zig Image Process",
-        vs.makeVersion(8, 0),
+        vs.makeVersion(9, 0),
         vs.VAPOURSYNTH_API_VERSION,
         0,
         plugin,
